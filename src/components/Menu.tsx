c2266d15c270514d12194760d8ab1c4217c2b385
Julian BARKOUDEH <Julian.barkoudeh@gmail.com>
2025-01-31 16:04:20 +0100

    nav bar + cards

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/Image";
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +114,28 @@ const menuItems = [
     ],
   },
 ];
+const Menu = () => {
+  return (
+    <div className="mt-4 text-sm ">
+      {menuItems.map((i) => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {i.title}
+          </span>
+          {i.items.map((item) => (
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center- justify-center gap-4 text-sm text-gray-500 mb-1 py-2 lg:justify-start"
+            >
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
